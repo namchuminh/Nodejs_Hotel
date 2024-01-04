c2c8a4aa5159eae90fe085df6fc9324dc7704619
Nam Chu Minh <chuminhnamma@gmail.com>
2024-01-04 18:30:36 +0700

    hoan thanh tin tuc khach hang

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,6 +5,7 @@ const roomRoute = require("./admin/room.routes");
 const webCustomerRoute = require("./website/customer.routes");
 const webRoomRoute = require("./website/room.routes");
 const webOrderRoute = require("./website/order.routes");
+const webNewsRoute = require("./website/news.routes");
 
 function route(app){
     app.use("/admin/category", categoryRoute);
@@ -13,6 +14,7 @@ function route(app){
     app.use("/phong-nghi", webRoomRoute);
     app.use("/khach-hang", webCustomerRoute);
     app.use("/dat-phong", webOrderRoute);
+    app.use("/tin-tuc", webNewsRoute);
 }
 
 module.exports = route
